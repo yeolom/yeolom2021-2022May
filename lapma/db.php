ca69afee220b6820a964883ed536d3d361b9a568
yeolomlead <105809524+yeolomlead@users.noreply.github.com>
2023-04-12 09:29:01 +0100

    Update db.php

--- lapma/db.php
+++ lapma/db.php
@@ -1,31 +1,18 @@
 <?php
-// //Local workspace.
-// $server = "localhost";
-// $user = "root";
-// $password = "";
-// $database = "lapma";
+//Local workspace.
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "lapma";
 
-// $conn = mysqli_connect($server, $user, $password, $database);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-
-//Cloud workspace.
-
- $server = "localhost";
- $user = "yeolomco_root";
- $password = "Olom@l123";
- $database = "yeolomco_lapma";
-
- $conn = mysqli_connect($server, $user, $password, $database);
+$conn = mysqli_connect($server, $user, $password, $database);
 
 // Check connection
- if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+
 
 
 ?>
