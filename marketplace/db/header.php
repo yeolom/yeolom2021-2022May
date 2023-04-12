f8321bb571d54114b1a229b31cc64f16a09bd955
yeolomlead <105809524+yeolomlead@users.noreply.github.com>
2023-04-12 09:28:27 +0100

    Update header.php

--- marketplace/db/header.php
+++ marketplace/db/header.php
@@ -1,14 +1,1 @@
-<?php
-    $server = "localhost";
-    $user = "yeolomco_root";
-    $password = "Olom@l123";
-    $database = "yeolomco_marketplace";
 
-    $conn = mysqli_connect($server, $user, $password, $database);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-?>
