1815c83bd327a38648969f63b8931e27777c29c2
yeolomlead <105809524+yeolomlead@users.noreply.github.com>
2023-04-12 09:30:40 +0100

    Update listCont.php

--- listCont.php
+++ listCont.php
@@ -1,31 +1,17 @@
 <?php
-// //Local workspace.
-// $server = "localhost";
-// $user = "root";
-// $password = "";
-// $database = "lapma";
+//Local workspace.
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "lapma";
 
-// $conn = mysqli_connect($server, $user, $password, $database);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-
-//Cloud workspace.
-
- $server = "localhost";
- $user = "yeolomco_root";
- $password = "Olom@l123";
- $database = "yeolomco_emailListing";
-
- $conn = mysqli_connect($server, $user, $password, $database);
+$conn = mysqli_connect($server, $user, $password, $database);
 
 // Check connection
- if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
 
 
 if((!isset($_POST['email']))||($_POST['email'] == NULL)){
